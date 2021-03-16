db62338053d1e8882af52b7a6e0de1e17fe3a362
VBalazs <v.vbalazs@gmail.com>
2021-03-17 00:46:27 +0100

    benne van a 2. feladat és úgy mőködik jelenleg ,hogy van benne egy tetsz adat ,hogy lefordúljon a program

--- src/mathematical_teamwork/Mathematical_teamwork.java
+++ src/mathematical_teamwork/Mathematical_teamwork.java
@@ -5,17 +5,50 @@
  */
 package mathematical_teamwork;
 
-/**
- *
- * @author Varga Balázs
- */
+import java.util.Stack;
+
 public class Mathematical_teamwork {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static String bementAdat;
+    private static Stack<String> veremBol;
+    private static String lengyelForma = "933*/";
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        veremBol = new Stack<>();
+        bementAdat = "9/(3*3)";
+        System.out.println(lengyelFormaSzamol(lengyelForma));
     }
-    
+
+    private static double lengyelFormaSzamol(String legyelformaBent) {
+        veremBol.clear();
+        while (!legyelformaBent.equals("")) {
+            char elem = legyelformaBent.charAt(0);
+            legyelformaBent = legyelformaBent.substring(1);
+            if (!(elem == '+' || elem == '-' || elem == '*' || elem == '/')) {
+                veremBol.add(elem + "");
+            } else {
+                int ertek1 = Integer.parseInt(veremBol.pop());
+                int ertek2 = Integer.parseInt(veremBol.pop());
+                int eredmeny = szamol(ertek2, elem, ertek1);
+                veremBol.add(eredmeny + "");
+            }
+        }
+        return Double.parseDouble(veremBol.pop());
+    }
+
+    private static int szamol(int ertek2, char elem, int ertek1) {
+        double eredmeny = 0;
+        if (elem == '+') {
+            eredmeny = ertek2 + ertek1;
+        } else if (elem == '-') {
+            eredmeny = ertek2 - ertek1;
+        } else if (elem == '*') {
+            eredmeny = ertek2 * ertek1;
+        } else if (elem == '/') {
+            eredmeny = ertek2 / ertek1;
+        }
+        return (int) eredmeny;
+
+    }
+
 }
